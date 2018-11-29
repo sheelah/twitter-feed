3526825bd9650f08c75ed7675f6c080115cec3d8
Sheelah Brennan <slb53@hotmail.com>
2018-11-28 19:31:33 -0700

    Update app component markup, styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components/macro';
+import styledNormalize from 'styled-normalize';
+import Header from './Header';
+import variables from './base/variables';
+
+const GlobalStyles = createGlobalStyle`
+  ${styledNormalize} // Normalize
+
+  h1,
+  h2,
+  h3,
+  h4 {
+    margin: 0;
+  }
+
+  ul {
+    margin: 0;
+  }
+`;
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <>
+        <GlobalStyles />
+        <ThemeProvider theme={variables}>
+          <div className="App">
+            <Header />
+          </div>
+        </ThemeProvider>
+      </>
+    );
   }
 }
 
