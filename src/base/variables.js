20bda92a9879bbb7bbe75d80e03a3cc01892e1ab
Sheelah Brennan <slb53@hotmail.com>
2018-11-28 19:31:03 -0700

    Add global variables

---
+++ src/base/variables.js
@@ -0,0 +1,13 @@
+/* Global variables that are made available to components via styled-components
+  See: https://www.styled-components.com/docs/tooling#themeprovider
+*/
+const variables = {
+  // Colors
+  blue: '#1da1f2',
+  white: '#ffffff',
+
+  // Grid
+  maxWidth: '1400px'
+};
+
+export default variables;
